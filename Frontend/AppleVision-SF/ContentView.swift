c579afc50018006da0a0988f45334521f7d5cebb
Queng123 <quentin.brejoin@gmail.com>
2024-11-05 15:27:40 -0800

    add packages

--- Frontend/AppleVision-SF/ContentView.swift
+++ Frontend/AppleVision-SF/ContentView.swift
@@ -7,7 +7,8 @@
 
 import SwiftUI
 import RealityKit
-import RealityKitContent
+import MapKit
+
 
 struct ContentView: View {
 
@@ -15,21 +16,7 @@ struct ContentView: View {
 
     var body: some View {
         VStack {
-            RealityView { content in
-                // Add the initial RealityKit content
-                if let scene = try? await Entity(named: "Scene", in: realityKitContentBundle) {
-                    content.add(scene)
-                }
-            } update: { content in
-                // Update the RealityKit content when SwiftUI state changes
-                if let scene = content.entities.first {
-                    let uniformScale: Float = enlarge ? 1.4 : 1.0
-                    scene.transform.scale = [uniformScale, uniformScale, uniformScale]
-                }
-            }
-            .gesture(TapGesture().targetedToAnyEntity().onEnded { _ in
-                enlarge.toggle()
-            })
+            Map()
 
             VStack {
                 Button {
