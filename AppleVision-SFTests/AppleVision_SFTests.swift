e312274db6eb330ada8ea52dcac29056ee20cb47
Queng123 <quentin.brejoin@gmail.com>
2024-11-04 22:30:30 -0800

    add base project

---
+++ AppleVision-SFTests/AppleVision_SFTests.swift
@@ -0,0 +1,17 @@
+//
+//  AppleVision_SFTests.swift
+//  AppleVision-SFTests
+//
+//  Created by Quentin Brejoin on 11/4/24.
+//
+
+import Testing
+@testable import AppleVision_SF
+
+struct AppleVision_SFTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
