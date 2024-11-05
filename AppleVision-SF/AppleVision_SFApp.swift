b4971014f27a863eea728b06345d3a9d4c07b91e
Queng123 <quentin.brejoin@gmail.com>
2024-11-04 22:17:56 -0800

    add base project

---
+++ AppleVision-SF/AppleVision_SFApp.swift
@@ -0,0 +1,18 @@
+//
+//  AppleVision_SFApp.swift
+//  AppleVision-SF
+//
+//  Created by Quentin Brejoin on 11/4/24.
+//
+
+import SwiftUI
+
+@main
+struct AppleVision_SFApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .windowStyle(.volumetric)
+    }
+}
