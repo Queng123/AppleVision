ff94a4cb40d1098680a0be5cd4dca2e7934c693b
Queng123 <quentin.brejoin@gmail.com>
2024-11-26 17:13:07 -0800

    add markers + split code in folders

--- Frontend/AppleVision-SF/Event.swift
+++ Frontend/AppleVision-SF/Event.swift
@@ -5,11 +5,13 @@
 //  Created by Tom Backert on 26.11.24.
 //
 import Foundation
-
+import MapKit
 
 struct Event: Identifiable {
     let id = UUID()
     let title: String
-    let location: String
+    let location: CLLocationCoordinate2D
+    let description: String
+    let imageMap: String
     let date: String
 }
---
+++ Frontend/AppleVision-SF/MapView.swift
@@ -0,0 +1,28 @@
+//
+//  MapView.swift
+//  AppleVision-SF
+//
+//  Created by Quentin Brejoin on 11/26/24.
+//
+
+import SwiftUI
+import MapKit
+
+struct MapView: View {
+    @Binding var events: [Event]
+
+    @State private var region = MapCameraPosition.region(MKCoordinateRegion(
+        center: CLLocationCoordinate2D(latitude: 37.7749, longitude: -122.4194),        span: MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1))
+    )
+    
+    var body: some View {
+            Map(position: $region) {
+                ForEach(events) { event in
+                    Marker(event.title, coordinate: event.location)
+                }
+
+            }
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .edgesIgnoringSafeArea(.all)
+        }
+}
